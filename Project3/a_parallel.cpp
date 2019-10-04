d9df784477661600f1ee47fbf93983fb226ed2e0
schraderSimon <simonsch@student.matnat.uio.no>
2019-10-04 17:31:16 +0200

    Added first draft of a parallelized program

--- Project3/a_parallel.cpp
+++ Project3/a_parallel.cpp
@@ -1,5 +1,5 @@
 #include "lib.h"
-
+#include <mpi.h>
 #include <math.h>
 #include <iomanip>
 #include <iostream>
@@ -21,41 +21,44 @@ double int_func(double x1, double x2, double y1, double y2, double z1, double z2
   return returnval;
 }
 int main(int argc, char** argv){
-  int N=atoi(argv[1]);
+  int N;
+  N=atoi(argv[1]);
   double *x = new double[N];
   double *w=new double[N];
-  double *x2=new double[N];
-  double *w2=new double[N];
   double lambda=3;
-  double int_gauss=0.;
-  double int_gauss2=0.;
   double add_var;
-  double add_var2=0;
   gauleg(-lambda,lambda,x,w,N); // Calls the lib.cpp function gauleg // THIS  LOOKS LIKE GULAG
-  gauleg(-lambda-1,lambda-1,x2,w2,N);
-  for(int i=0; i<N;i++){
-    cout<<w[i]<< " "<<x[i]<<endl;
-  }
-  cout <<endl;
-  for(int i=0; i<N;i++){
-    cout<<w2[i]<< " "<<x2[i]<<endl;
-  }
-  for (int i=0;i<N;i++){
+  int local_n,numprocs,my_rank;
+  double time_start,time_end,total_time;
+  double local_sum;
+  double total_sum;
+  int i;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
+  MPI_Comm_rank (MPI_COMM_WORLD,&my_rank);
+  time_start=MPI_Wtime();
+  total_sum=0;
+  i=my_rank;
     for (int j = 0;j<N;j++){
       for (int k = 0;k<N;k++){
         for (int l = 0;l<N;l++){
            for (int m = 0;m<N;m++){
               for (int n = 0;n<N;n++){
-                add_var=w[i]*w[j]*w[k]*w[l]*w[m]*w[n]*int_func(x[i],x[j],x[k],x[l],x[m],x[n]);
+                local_sum+=w[i]*w[j]*w[k]*w[l]*w[m]*w[n]*int_func(x[i],x[j],x[k],x[l],x[m],x[n]);
                 /*This is clearly only allowed because all 6 dimensions have the same dimensionality */
                 //cout <<"val of addvar " <<add_var<<endl;
-                int_gauss+=add_var;
      		      }
             }
         }
       }
     }
+  MPI_Reduce(&local_sum,&total_sum,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
+  time_end=MPI_Wtime();
+  total_time=time_end-time_start;
+  if (my_rank==0){
+    cout<<"total sum:" << total_sum << endl;
+    cout << "Time = " <<  total_time << " on number of processors: "  << numprocs  << endl;
+    cout << g << endl;
   }
-  cout <<" approach 1:"<< int_gauss << " approach 2: "<< int_gauss2<< " correct: "<< 5*3.14159265359*3.14159265359/(16*16)<< endl;
-  cout << g << endl;
+  MPI_Finalize ();
 }
