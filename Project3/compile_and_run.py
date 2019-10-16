54dc294e07a0df23ead0ac20bd36542b5d7ba8ff
schraderSimon <simonsch@student.matnat.uio.no>
2019-10-16 18:32:22 +0200

    Added some plotting and file reading

--- Project3/compile_and_run.py
+++ Project3/compile_and_run.py
@@ -2,9 +2,9 @@ import subprocess, sys
 import numpy as np
 filenames=["a","b","c","d","a_parallel","b_parallel","c_parallel","d_parallel"];
 helpfiles=["lib","functions","tests_main","testings"]
-n_gaussisk=[10,15,20]
-n_gaussisk_parallel=[10,15,20,25,30,40]
-n_monte=[1000,10000,100000]
+n_gaussisk=[10,15,20,30]
+n_gaussisk_parallel=[10,15,20,25,30,40,45]
+n_monte=[1000,10000,100000,1000000,1000000,1000000,10000000]
 n_monte_parallel=[1000,10000,100000,100000,1000000,10000000,100000000,1000000000]
 for i in range(int(len(filenames)/2)):
     subprocess.call("c++ -c "+filenames[i]+".cpp",shell=True)
@@ -30,7 +30,6 @@ def compile(fast):
         subprocess.call("mpic++ "+fast+" -o "+filenames[i]+".exe "+filenames[i]+".o lib.o functions.o",shell=True)
     for i in range(int(3*len(filenames)/4),len(filenames)):
         subprocess.call("mpic++ "+fast+" -o "+filenames[i]+".exe "+filenames[i]+".o functions.o",shell=True)
-compile("-O3")
 def runAll(mengde):
     for amount in range(mengde):
         for n in n_gaussisk:
---
+++ Project3/databehandling.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+infile=np.loadtxt("results/time_info.csv",delimiter=",",dtype="str")
+outfile1=open("results/accuracy_time.csv","w")
+gauleg=infile[infile[:,0]=="gauleg"]
+gaulag=infile[infile[:,0]=="gauleg"]
+monteBad=infile[infile[:,0]=="badMonteCarlo"]
+monteGood=infile[infile[:,0]=="goodMonteCarlo"]
+monteGood_n=np.unique(monteGood[:,3]).astype(np.int) #N values
+monteGood_vectorized_parallel=monteGood[np.logical_and(monteGood[:,1]=="yes",monteGood[:,2]=="4")]
+monteGood_vectorized_noparallel=monteGood[np.logical_and(monteGood[:,1]=="yes",monteGood[:,2]=="1")]
+monteGood_novectorized_parallel=monteGood[np.logical_and(monteGood[:,1]=="no",monteGood[:,2]=="4")]
+monteGood_novectorized_noparallel=monteGood[np.logical_and(monteGood[:,1]=="no",monteGood[:,2]=="1")]
+#time_vectorized_parallel=np.avg()
+time_vectorized_parallel=[]
+time_vectorized_noparallel=[]
+time_novectorized_parallel=[]
+time_novectorized_noparallel=[]
+print(monteGood_vectorized_noparallel)
+monteGood_n_short=np.unique(monteGood_novectorized_noparallel[:,3]).astype(np.int)
+for n in monteGood_n_short:
+    time_vectorized_noparallel.append(np.mean(monteGood_vectorized_noparallel[monteGood_vectorized_noparallel[:,3]==str(n)][:,4].astype(np.float)))
+    time_novectorized_noparallel.append(np.mean(monteGood_novectorized_noparallel[monteGood_novectorized_noparallel[:,3]==str(n)][:,4].astype(np.float)))
+for n in monteGood_n:
+    time_vectorized_parallel.append(np.mean(monteGood_vectorized_parallel[monteGood_vectorized_parallel[:,3]==str(n)][:,4].astype(np.float)))
+    time_novectorized_parallel.append(np.mean(monteGood_novectorized_parallel[monteGood_novectorized_parallel[:,3]==str(n)][:,4].astype(np.float)))
+print(monteGood_n_short)
+plt.plot(monteGood_n_short,time_vectorized_noparallel,label="time_vectorized_noparallel")
+plt.plot(monteGood_n_short,time_novectorized_noparallel,label="time_novectorized_noparallel")
+plt.plot(monteGood_n[:6],time_vectorized_parallel[:6],label="time_vectorized_parallel")
+plt.plot(monteGood_n[:6],time_novectorized_parallel[:6],label="time_novectorized_parallel")
+plt.xscale("log")
+#plt.yscale("log")
+plt.legend()
+plt.show()
+plt.title("Relative difference in time use as function of n")
+plt.xlabel("N")
+plt.ylabel(r"Relative difference $\frac{nonparallelized}{parallelized}$")
+plt.xscale("log")
+rel=np.asarray(time_novectorized_noparallel)/np.asarray(time_novectorized_parallel[:6])
+print(np.asarray(time_novectorized_noparallel))
+print(np.asarray(time_novectorized_parallel[:6]))
+print(monteGood_n[:6],rel)
+plt.plot(monteGood_n[:6],rel)
+plt.savefig("Relative_difference_time.png")
+plt.show()
