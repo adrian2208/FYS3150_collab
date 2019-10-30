8c7703a5adc5a6e04237b16f258ec37020bdc379
schraderSimon <simonsch@student.matnat.uio.no>
2019-10-30 13:55:22 +0100

    kind of finished (not tested) b

--- Project4/b.cpp
+++ Project4/b.cpp
@@ -1,1 +1,124 @@
 //more to add here
+#include "vecop.hpp"
+#include <iostream>
+#include <random>
+#include <cmath>
+using namespace std;
+int getPeriodic(int i, int n){
+  return (i+n)%n;
+}
+int** setUpRandomMatrix(int n){
+  std::random_device rd;
+  std::mt19937_64 gen(rd()); //Each thread gets a different seed, as the rank is included
+  std::uniform_real_distribution<double> RnG(0.0,1.0);
+  int **A=createNNMatrix_int(n);
+  for (int i=0; i<n; i++){
+    for(int j=0;j<n;j++){
+      if (RnG(gen)>0.5){
+        A[i][j]=1;
+      }
+      else{
+        A[i][j]=-1;
+      }
+    }
+  }
+  return A;
+}
+
+
+int findStartEnergy(int** A, int n){
+  int tot_eng=0; // Total energy
+  for (int i=0;i<n-1;i++){
+    /*Calculate the energy of the neighbours under for each row*/
+    for (int j=0;j<n-1;j++){
+      tot_eng+=A[i][j]*A[i+1][j];
+    }
+  }
+  for (int i=0;i<n-1;i++){
+    /*Calculate the energy of the neighbours to the right for each row*/
+    for (int j=0;j<n-1;j++){
+      tot_eng+=A[i][j]*A[i][j+1];
+    }
+  }
+  for(int i=0;i<n-1;i++){
+    /*Calculate last row and last column, but not boundary conditions*/
+    tot_eng+=A[i][n-1]*A[i+1][n-1];
+    tot_eng+=A[n-1][i]*A[n-1][i+1];
+  }
+  for (int i=0;i<n;i++){
+    /*Calculate the energy due to boundary conditions*/
+    tot_eng+=A[i][n-1]*A[i][0];
+    tot_eng+=A[n-1][i]*A[0][i];
+  }
+  return -tot_eng;
+}
+int findStartMagnetization(int** A, int n){
+  int tot_mag=0;
+  for (int i=0;i<n;i++){
+    for (int j=0;j<n;j++){
+      tot_mag+=A[i][j];
+    }
+  }
+  return tot_mag;
+}
+/*
+void random_permute(int** A,int n,int* swap_positions,double* RnG(mt19937_64),mt19937_64* gen){
+  swap_positions[0]=(int) (((*RnG)(*gen)) * n);
+  swap_positions[1]=(int) (((*RnG)(*gen)) * n);
+  A[swap_positions[0]][swap_positions[1]]*=-1;
+}
+*/
+int main(int argc, char** argv){
+  std::random_device rd;
+  std::mt19937_64 gen(rd());
+  std::uniform_real_distribution<double> RnG(0.0,1.0);
+  clock_t start, finish; //Start and end time
+  int n;
+  double temp;
+  long long int amount;
+  //ofstream outfile;
+  if(argc>=4){
+    amount=atoi(argv[1]);
+    n=atoi(argv[2]);
+    temp=atof(argv[3]);
+  }
+  else{
+    cout << "You need to state the amount of iterations, the matrix dimension and the temperature" << endl;
+    exit(1);
+  }
+  double exponents[17];
+  for(int i=-8;i<=8;i+=4){
+    exponents[i+8]=exp(-i/temp);
+  }
+  int ** A=setUpRandomMatrix(n);
+  /*
+  for (int i=0; i<n;i++){
+    for(int j=0;j<n;j++){
+      cout << A[i][j] << " ";
+    }
+    cout << endl;
+  }*/
+  int magnet=findStartMagnetization(A, n);
+  int energy=findStartEnergy(A, n);
+  int swap_i,swap_j;
+  int deltaE,deltaM,newSpin;
+  double result_energy=0,result_magnet=0,result_energySquared=0,result_magnetSquared=0,result_magnetAbs=0;
+
+  for(int i=0;i<amount;i++){
+    swap_i=(int)(n*RnG(gen));
+    swap_j=(int)(n*RnG(gen));
+    deltaE=2*A[swap_i][swap_j]*(A[getPeriodic(swap_i+1,n)][swap_j]+A[getPeriodic(swap_i-1,n)][swap_j]+A[swap_i][getPeriodic(swap_j-1,n)]+A[swap_i][getPeriodic(swap_j+1,n)]);
+    newSpin=-A[swap_i][swap_j];
+    deltaM=2*newSpin;
+    if(exponents[deltaE+8]>=RnG(gen)){
+      A[swap_i][swap_j]*=-1;
+      magnet+=deltaM;
+      energy+=deltaE;
+    }
+    result_energy+=energy;
+    result_energySquared+=energy*energy;
+    result_magnetSquared+=magnet*magnet;
+    result_magnetAbs+=fabs(magnet);
+  }
+  cout <<"Absolute magnet: " <<result_magnetAbs/amount <<"Magnet: " <<result_magnet/amount << "Energy: " << result_energy/amount<<endl;
+}
---
+++ Project4/vecop.hpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <iomanip>
+using namespace std;
+int** createNNMatrix_int(int n);
+void deleteNNMatrix_int(int** A,int n);
+string createFileName(string name,double n); // creates a string in the form *name*_*n*.txt
+double* createArray(int n); //Creates an Array of length n, filled with zeros
+void fillArray(double* arr,int n,double num); //fills an array (of length n) (by reference) with a given number (num)
+void fillArrayFunction(double * arr, int n,double *x,double (*func)(double)); //same as fillArray, but doesnt take an umber, but a generic function
+double func(double x); //The function we have to double integrate
+void solve(double *a, double *b, double*c, double *deriv, int n,double *solutions); //The actual solving algorithm for a general triangular matrix does changes to "solutions" (with length n+2)
+/* Oppgave c start */
+void improvedSolve(double *deriv,double *b, int n, double *solutions); // The solving algorithm when a = c=-1 and b=2
+/*Oppgave e start */
+double** createNNMatrix(int n); //Creates a double two-pointer of size n*n (that is, an n*n matrix)
+void deleteNNMatrix(double** A,int n); //Deletes a matrix created by createNNMatrix
+double ** createJacobiMatrix(int n, int i, int j, double theta); // Creates a Jacobi matrix
+void invertJacobiMatrix(double **S,int n, int i, int j); //Inverts an existing Jacobi Matrix (that is, S is being overwritten)
+double ** matrixMult(int n, double **A, double **B);
+void jacobi_diag(double** A, double **R, int n, double tol);
+double* findMax(double **A, int n); // returns an array where arr[0] is the largest value, arr[1] the i element and arr[2] the j element
+void printMatrix(double **A, int n);
+void jacobi_diag(double **A,double **R, int n, double tol);
+int jacobi_diag2(double **A,double **R, int n, double tol);
+
+double ** copyMatrix(double**A,int n);
