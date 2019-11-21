90d04c94133619578144ae0b2088ba49c59dca61
schraderSimon <simonsch@student.matnat.uio.no>
2019-11-21 13:46:52 +0100

    Folder creation, outline of object oriented program

---
+++ Project5/cpp/solver.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <string>
+using namespace std;
+class Solver{
+  private:
+    double alpha;
+    double beta;
+    double omega;
+  public:
+    Solver(double alpha, double beta){
+       this->alpha = alpha;
+       this->beta = beta;
+       this->omega = omega;
+    }
+    double El1(){
+
+    }
+    double function(){
+
+    }
+    double EL2(){
+
+    }
+
+};
+int main(int argc, char** argv){
+}
